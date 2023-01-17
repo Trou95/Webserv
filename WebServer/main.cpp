0ba48920afdab9ffaaad86aa2ad7c642c6962b4c
Gorkem95 <gorkem_095@hotmail.com>
2023-01-17 23:49:42 +0300

    Update

--- WebServer/main.cpp
+++ WebServer/main.cpp
@@ -5,14 +5,12 @@
 #include "Utils/FileReader/FileReader.h"
 #include "Utils/Parser/Request/RequestParser.h"
 
+#include <fstream>
 
 
 int main(int ac, char** av)
 {
     Malazgirt malazgirt(av[1] ? av[1] : "Conf/default.conf");
-
-
-
 
 
     return 0;
--- WebServer/www/web/test/upload.php
+++ WebServer/www/web/test/upload.php
@@ -2,4 +2,6 @@
 
 print_r($_POST);
 
+print_r($_FILES);
+
 ?>
